e60f323dd546bbefeef0b9c3642c5b57d4a6a371
IvanR96 <irios96@hotmail.com>
2025-11-17 22:51:01 -0800

    reminder on attributes for styles React

--- expression-practice/src/main.jsx
+++ expression-practice/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 const name = "Ivan";
 const currentYear = new Date().getFullYear();
 
+//any style attributes camelCase in React
 createRoot(document.getElementById('root')).render(
   <div>
     <h1 className='title'>JSX Expression Practice</h1>

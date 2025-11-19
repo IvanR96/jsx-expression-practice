426946eab6a18adbe9a7ebfb35aff8a117816a4e
IvanR96 <irios96@hotmail.com>
2025-11-19 01:00:11 -0800

    inline custom style added to main jsx

--- expression-practice/src/main.jsx
+++ expression-practice/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 
 const name = "Ivan";
 const currentYear = new Date().getFullYear();
+const customStyle = {
+  color: "red",
+}
 
 //any style attributes camelCase in React
 createRoot(document.getElementById('root')).render(
   <div>
-    <h1 className='title' contentEditable="true" spellCheck="false" >JSX Expression Practice</h1>
+    <h1 style={customStyle}>JSX Expression Practice</h1>
     <img className='small' src="https://images.unsplash.com/photo-1585247226801-bc613c441316?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHwyfDB8fHwy" alt="computer-desk" />
     <img className='small' src="https://images.unsplash.com/photo-1613253932202-686cbcd993b0?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHwyfDB8fHwy" alt="books" />
     <img className='small' src="https://images.unsplash.com/photo-1493020258366-be3ead1b3027?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8MnwwfHx8Mg%3D%3D" alt="image-laptop" />
